a67a379eb2e8e53ca8ab8335c113122d05b95691
o-chan-1998 <root@localhost.localdomain>
2022-04-16 23:40:19 -0400

    vector型

--- sample_work_001/src/main.rs
+++ sample_work_001/src/main.rs
@@ -4,23 +4,11 @@ fn main() {
 }
 
 pub fn run() {
-    let a1: [u8; 7000000] = [1; 7000000];
+    let v1 = vec![1, 2, 3, 4];
+    let v2 = vec![5, 6, 7, 8];
+    println!("Stack address of v1 is: {:p}", &v1);  // Stack address of v1 is: 0x7ffe5b788440
+    println!("Stack address of v2 is: {:p}", &v2);  // Stack address of v2 is: 0x7ffe5b788458
+    println!("Heap memory address of v1: {:?}", v1.as_ptr());   // Heap memory address of v1: 0x557144e1eae0
+    println!("Len of v1 is: {}", v1.len()); // Len of v1 is: 4
+    println!("Capacity of v1 is: {}", v1.capacity());   // Capacity of v1 is: 4
 }
-
-// [root@localhost sample_work_001]# cargo run
-// warning: unused variable: `a1`
-//  --> src/main.rs:7:9
-//   |
-// 7 |     let a1: [u8; 9000000] = [1; 9000000];
-//   |         ^^ help: if this is intentional, prefix it with an underscore: `_a1`
-//   |
-//   = note: `#[warn(unused_variables)]` on by default
-
-// warning: `sample_work_001` (bin "sample_work_001") generated 1 warning
-//     Finished dev [unoptimized + debuginfo] target(s) in 0.00s
-//      Running `target/debug/sample_work_001`
-// Hello, world!
-
-// thread 'main' has overflowed its stack
-// fatal runtime error: stack overflow
-// 中止 (コアダンプ)
